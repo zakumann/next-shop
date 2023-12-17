59de6acfb0ab1d25bdd64330f193ee5ad42bc8ce
zakumann <samuel742able@gmail.com>
2023-12-17 23:42:30 +0900

    12172023

---
+++ components/Title.js
@@ -0,0 +1,7 @@
+export default function Title({ children }){
+    return(
+        <h1 className="text-2xl pb-4">
+            {children}
+        </h1>
+    );
+}
--- pages/index-1.js
+++ pages/index-1.js
@@ -1,12 +1,16 @@
+// Option 1: fetch products on the server side(in getStaticProps)
 import Head from 'next/head';
 import Title from '../components/Title';
+import { getProducts } from '../lib/products';
 
-const products = [
-  { id: 1, title: 'First Product' },
-  { id: 2, title: 'Second Product' },
-];
+export async function getStaticProps(){
+    console.log('[HomePage] getStaticProps()');
+    const products = await getProducts();
+    return { props: { products } };
+}
 
-export default function HomePage() {
+export default function HomePage({ products}) {
+    console.log('[HomePage] render:', products);
   return (
     <>
       <Head>
